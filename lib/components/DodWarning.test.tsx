0bea01740bf898a0297d6ce229444e745cd0bba8
John Edward Willman V <john.willman.1@us.af.mil>
2025-03-22 15:06:53 -0400

    moved title into const

--- lib/components/DodWarning.test.tsx
+++ lib/components/DodWarning.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import DodWarning from "./DodWarning";
 
+const dodWarningTitle: string = "USG Warning and Consent Banner"
 
 beforeEach(() => {
     sessionStorage.clear();
@@ -9,26 +10,26 @@ beforeEach(() => {
 
 test("renders the modal when sessionStorage does not have lastVisit set to today", () => {
     render(<DodWarning />);
-    expect(screen.getByText("USG Warning and Consent Banner"));
+    expect(screen.getByText(dodWarningTitle));
     expect(document.body.classList.contains("no-interaction")).toBe(true);
 });
 
 test("does not render the modal if sessionStorage has today's date", () => {
     sessionStorage.setItem("lastVisit", new Date().toDateString());
     render(<DodWarning />);
-    expect(screen.queryByText("USG Warning and Consent Banner")).toBeNull();
+    expect(screen.queryByText(dodWarningTitle)).toBeNull();
     expect(document.body.classList.contains("no-interaction")).toBe(false);
 });
 
 test("clicking OK button closes the modal and updates sessionStorage", () => {
     render(<DodWarning />);
     const button = screen.getByText("OK");
-    expect(screen.getByText("USG Warning and Consent Banner"));
+    expect(screen.getByText(dodWarningTitle));
     expect(document.body.classList.contains("no-interaction")).toBe(true);
 
     fireEvent.click(button);
     
-    expect(screen.queryByText("USG Warning and Consent Banner")).toBeNull();
+    expect(screen.queryByText(dodWarningTitle)).toBeNull();
     expect(sessionStorage.getItem("lastVisit")).toBe(new Date().toDateString());
     expect(document.body.classList.contains("no-interaction")).toBe(false);
 });
